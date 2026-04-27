aaf8f62b22b956159775cdb1f3365a263ad9c52a
aaltsant <santeri.aaltonen@tuni.fi>
2026-04-27 13:06:33 +0300

    feat(Results.jsx): result to frontend

--- frontend/RuuviTag/src/app/Results.jsx
+++ frontend/RuuviTag/src/app/Results.jsx
@@ -10,6 +10,27 @@ function Results() {
         style={{ width: '80%', height: '500px', border: 'none' }}
         title="Saunadata"
       />
+
+      <h2>Tähän joku uusi:</h2>
+      <iframe
+        src="/sauna6342.html"
+        style={{ width: '80%', height: '500px', border: 'none' }}
+        title="Saunadata"
+      />
+
+      <h2>Tähän joku uusi:</h2>
+      <iframe
+        src="/saunomiskerta.html"
+        style={{ width: '80%', height: '500px', border: 'none' }}
+        title="Saunadata"
+      />
+
+      <h2>Kaavio yhdestä saunakerrasta:</h2>
+      <iframe
+        src="/kesa_talvi.html"
+        style={{ width: '80%', height: '500px', border: 'none' }}
+        title="kesätalvisauna"
+      />
     </div>
   );
 
